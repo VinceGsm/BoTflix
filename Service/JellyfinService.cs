75f70f69f9b503c49510a7db4d4976068c097996
VinceGsm <v.gusmini@outlook.fr>
2023-05-17 18:02:30 +0200

    remove ClearChannel

--- Service/JellyfinService.cs
+++ Service/JellyfinService.cs
@@ -1,6 +1,4 @@
-﻿using Discord;
-using Discord.WebSocket;
-using NgrokApi;
+﻿using NgrokApi;
 using System.Diagnostics;
 
 namespace BoTflix.Service
@@ -8,30 +6,7 @@ namespace BoTflix.Service
     public class JellyfinService
     {
         private static readonly string _ngrokBatPath = @"C:\Program Files\Ngrok\ngrok.bat";
-        private static readonly string _jellyfinPath = @"C:\Program Files\Jellyfin\jellyfin_10.8.4\jellyfin.exe";
-        private List<IMessage> _toDelete = new List<IMessage>();
-
-
-        /// <summary>        
-        /// Clean the old links
-        /// </summary>
-        /// <returns></returns>
-        internal async Task ClearChannel(DiscordSocketClient client)
-        {
-            IAsyncEnumerable<IReadOnlyCollection<IMessage>> messages;
-            var channel = Helper.GetSocketMessageChannel(client, 816283362478129182); //Jellyfin
-
-            if (channel != null)
-            {
-                _toDelete.Clear();
-
-                messages = channel.GetMessagesAsync(50); //recover the last 50 msg                
-                FillMsgList(messages);
-
-                if (_toDelete.Count > 0)
-                    foreach (var msg in _toDelete) await channel.DeleteMessageAsync(msg);
-            }
-        }
+        private static readonly string _jellyfinPath = @"C:\Program Files\Jellyfin\jellyfin_10.8.4\jellyfin.exe";        
 
         internal async Task<string> GetNgrokUrl()
         {
@@ -62,24 +37,6 @@ namespace BoTflix.Service
             {
                 Helper.StartProcess(_jellyfinPath);
                 Thread.Sleep(4000); // wait 4sec
-            }
-        }
-
-
-        /// <summary>
-        /// Select message about Jellyfin link
-        /// </summary>
-        /// <param name="messages"></param>
-        private void FillMsgList(IAsyncEnumerable<IReadOnlyCollection<IMessage>> messages)
-        {
-            var msgAsync = messages.ToListAsync().Result;
-
-            foreach (var list in msgAsync)
-            {
-                IEnumerable<IMessage> msg = list.Where(x => x.Content.StartsWith("<a:pepeSmoke:830799658354737178>"));
-                IEnumerable<IMessage> msg2 = list.Where(x => x.Content.StartsWith("<a:luffy:863101041498259457>"));
-                _toDelete.AddRange(msg);
-                _toDelete.AddRange(msg2);
             }
         }
     }
